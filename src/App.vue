62e34046ad1c23be6e5a249c81c669b7e69a21d0
KendraLa <kendralambillon@gmail.com>
2024-01-27 19:33:33 +0100

    Display all sotred data

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,7 @@
 
   onMounted(()=> {
     name.value = localStorage.getItem('name') || ''
+    todos.value = JSON.parse(localStorage.getItem('todos'))
   })
 </script>
 
@@ -67,7 +68,22 @@
         <input type="submit" value="Add to do">
       </form>
 
-      {{ input_content }}
+      {{ todos }}
+    </section>
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list">
+        <div v-for="todo in todos" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done">
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content">
+          </div>
+        </div>
+      </div>
     </section>
   </main>
 </template>
