b905973073fd0c3390eaea39d5f5715787bafd9a
KendraLa <kendralambillon@gmail.com>
2024-01-27 18:44:39 +0100

    ref, watch, onMounted, v-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup>
+  import { ref, watch, onMounted } from 'vue'
 
+  const todos = ref([])
+  const name = ref('')
+
+  watch(name, (newVal) => {
+    localStorage.setItem('name', newVal)
+  })
+
+  onMounted(()=> {
+    name.value = localStorage.getItem('name') || ''
+  })
 </script>
 
 <template>
-
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="Name here" v-model="name">
+      </h2>
+    </section>
+  </main>
 </template>
 
 
